ea70f10943588a7d6964d2e40e280ac17feef476
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-10-18 21:52:48 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+
+import { useEffect, useState } from "react";
+
+export default function App() {
+    const [image, setImage] = useState(null)
+
+        useEffect(() => {
+            fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(data => setImage(data.message) )
+        }, [])
+
+        if(!image){
+            return <p>Loading...</p>
+        }
+    return (
+        <>
+            <h1>Simple Data Fetching Lab</h1>
+            <img src={image} alt='A Random Dog' />
+        </>
+
+    );
+}
